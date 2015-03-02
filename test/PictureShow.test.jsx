fcc203ff9cf3ae987148792bd49e21a93ae54a75
Greg Skiano <greg.skiano@dowjones.com>
2015-03-02 15:16:33 -0500

    moving a actual test from old jest tests

--- test/PictureShow.test.jsx
+++ test/PictureShow.test.jsx
@@ -2,18 +2,41 @@
 var createDOM = require('../util/createDOM'),
   PictureShow = require('../PictureShow'),
   React = require('react/addons'),
+  assert = require('should'),
   TestUtils = React.addons.TestUtils;
 
-describe('PictureShow', function () {
+var slideshowElm, testSlides;
 
-  beforeEach(createDOM);
+describe('PictureShow Structure', function () {
 
-  it('should do something', function () {
+  beforeEach(function () {
 
-    var el = (<PictureShow slides={[]}/>);
-    var mounted = TestUtils.renderIntoDocument(el);
+    createDOM();
 
-    console.log(mounted);
+    testSlides = ['A','B','C','D'].map(function (letter) {
+      return (<div className={letter}/>);
+    });
+
+    slideshowElm = (<PictureShow startingSlide={0} slides={testSlides} className='added-class'/>);
+
+  });
+
+  it('should add correct classes', function () {
+
+    var slideshow = TestUtils.renderIntoDocument(slideshowElm),
+      wrap,
+      panels;
+
+    slideshow.getDOMNode().className.should.match(/\bwsj-slideshow\b/);
+    slideshow.getDOMNode().className.should.match(/\badded-class\b/);
+
+    assert.doesNotThrow(function () {
+      wrap = TestUtils.findRenderedDOMComponentWithClass(slideshow, 'wsj-slideshow-wrap');
+    });
+
+    panels = TestUtils.scryRenderedDOMComponentsWithClass(wrap, 'wsj-slideshow-slides');
+
+    panels.length.should.equal(3);
 
   });
 
