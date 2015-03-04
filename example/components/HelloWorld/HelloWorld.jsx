0979ec25ce779d282f2b1af8dbb2ffabc52f29b7
Greg Skiano <gregoryskiano@gmail.com>
2015-03-04 08:42:49 -0500

    starting real examples

--- example/components/HelloWorld/HelloWorld.jsx
+++ example/components/HelloWorld/HelloWorld.jsx
@@ -1,13 +1,18 @@
 /** @jsx React.DOM */
 
 var React = require('react'),
+  PictureShow = require('../../../src/PictureShow.jsx'),
   HelloWorld;
 
 module.exports = HelloWorld = React.createClass({
   render: function () {
     return (
       <div className="HelloWorld">
-        <p>Hello world!</p>
+        <PictureShow ratio={[16,9]}>
+          <p>Hello world!</p>
+          <p>Hello world!</p>
+          <p>Hello world!</p>
+        </PictureShow>
       </div>
     );
   }
